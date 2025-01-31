712b0050571f0f5c9ee792077817af94ba451dfc
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-30 21:39:08 -0600

    Creating the Window class

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.root = Tk()
+        self.root.title("Maze Solver")
