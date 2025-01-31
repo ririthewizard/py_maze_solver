22e720ef1e5114ffcd484d58b6da86958382c6b4
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-30 21:44:59 -0600

    Using different callback functions to create a simple free drawing canvas with Tk

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from tkinter import *
 from tkinter import ttk
 
-class Window:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
+def save_posn(event):
+    global lastx, lasty
+    lastx, lasty = event.x, event.y
 
-        self.root = Tk()
-        self.root.title("Maze Solver")
+def add_line(event):
+    canvas.create_line((lastx, lasty, event.x, event.y))
+    save_posn(event)
+
+root = Tk()
+root.title("Balls")
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+canvas = Canvas(root)
+canvas.grid(column=0, row=0, sticky=(N, W, E, S))
+canvas.bind("<1>", save_posn)
+canvas.bind("<B1-Motion>", add_line)
+
+root.mainloop()
