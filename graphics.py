35f45ef68b0ea391fa5fe3634f2915124d696a83
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-30 22:22:12 -0600

    Working on the line class that will take 2 points and use those to draw on the canvas

---
+++ graphics.py
@@ -0,0 +1,39 @@
+from tkinter import *
+from tkinter import ttk
+
+class Window:
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        self.running = False
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+        print("Window closed...")
+
+    def close(self):
+        self.running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_a: Point, point_b: Point):
+        self.point_a = point_a
+        self.point_b = point_b
+    
