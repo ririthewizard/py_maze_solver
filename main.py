f8ec35949738eaacac50cf61ffa1d62ae6e02a3f
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-30 22:02:52 -0600

    Figuring out differences between pack and grid. Got a window up and running!

--- main.py
+++ main.py
@@ -3,8 +3,33 @@ from tkinter import ttk
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-
         self.root = Tk()
         self.root.title("Maze Solver")
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas.grid(column=0, row=0, sticky=(N, W, E, S))
+
+        self.running = False
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+        print("Window closed...")
+
+    def close(self):
+        self.running = False
+
+    
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+main()
