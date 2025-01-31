e90b76a52afefd8862f0bd4c0f899518b27fa9a7
Riley Williams <ririwilliams.rw@gmail.com>
2025-01-30 22:09:36 -0600

    Changing into separate classes

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
-from tkinter import *
-from tkinter import ttk
+from graphics import Window
 
-class Window:
-    def __init__(self, width, height):
-        self.root = Tk()
-        self.root.title("Maze Solver")
-        self.root.columnconfigure(0, weight=1)
-        self.root.rowconfigure(0, weight=1)
-        self.root.protocol("WM_DELETE_WINDOW", self.close)
-
-        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
-        self.canvas.grid(column=0, row=0, sticky=(N, W, E, S))
-
-        self.running = False
-
-    def redraw(self):
-        self.root.update_idletasks()
-        self.root.update()
-
-    def wait_for_close(self):
-        self.running = True
-        while self.running:
-            self.redraw()
-        print("Window closed...")
-
-    def close(self):
-        self.running = False
-
-    
 def main():
     win = Window(800, 600)
     win.wait_for_close()
