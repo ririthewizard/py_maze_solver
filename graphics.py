2a5369561f8583084889c0c7e968d13d55de5e58
Riley Williams <ririwilliams.rw@gmail.com>
2025-02-03 20:20:29 -0600

    Finished the Line class and added a draw_line method to window that we confirmed works in main! Now on to making cells

--- graphics.py
+++ graphics.py
@@ -9,10 +9,14 @@ class Window:
         self.root.rowconfigure(0, weight=1)
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
-        self.canvas = Canvas(self.root, bg="white", height=height, width=width)
+        self.canvas = Canvas(self.root, bg="white", width=width, height=height)
         self.canvas.grid(column=0, row=0, sticky=(N, W, E, S))
 
         self.running = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
 
     def redraw(self):
         self.root.update_idletasks()
@@ -36,7 +40,6 @@ class Line:
     def __init__(self, point_a: Point, point_b: Point):
         self.point_a = point_a
         self.point_b = point_b
-    
-    def draw(canvas: Canvas, fill_color):
-        canvas = Canvas(root)
-        
+
+    def draw(self, canvas: Canvas, fill_color):
+        canvas.create_line(self.point_a.x, self.point_a.y, self.point_b.x, self.point_b.y, fill=fill_color, width=2)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from graphics import Window
+from graphics import *
 
 def main():
-    win = Window(800, 600)
-    win.wait_for_close()
+    main_window = Window(800, 600)
+    l = Line(Point(400, 0), Point(400, 400))
+    l2 = Line(Point(400, 300), Point(400, 300))
+    main_window.draw_line(l, "black")
+    main_window.draw_line(l2, "black")
+
+    main_window.wait_for_close()
 
 main()
