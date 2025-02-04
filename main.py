61bf5cc27dff7e4435052e89ef696ea1c666229f
Riley Williams <ririwilliams.rw@gmail.com>
2025-02-04 12:08:57 -0600

    Ok now on to drawing cells. To the next branch!

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from graphics import *
 
 def main():
     main_window = Window(800, 600)
-    l = Line(Point(400, 0), Point(400, 400))
-    l2 = Line(Point(400, 300), Point(400, 300))
-    main_window.draw_line(l, "black")
-    main_window.draw_line(l2, "black")
+    
 
     main_window.wait_for_close()
 
